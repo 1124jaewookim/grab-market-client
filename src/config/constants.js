fc40d0fafe6260e46ac2e1911f5f49a4d0cd6f55
KimJaewoo <kljw01124@gm.gist.ac.kr>
2023-06-13 00:42:24 +0900

    url changed

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 // export const API_URL = "https://catch-market-server-230501.fly.dev";
 // export const API_URL = "http://localhost:8080";
 // export const WEB_URL = "https://freund.co.kr";
-export const API_URL = "https://catch-market-230608.fly.dev";
+export const API_URL = "https://catch-market-finaldemo.fly.dev";
